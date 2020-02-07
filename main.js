211e46c1cb337fad91ab54b22b30c1354616cd31
Anna Mazurek <anna.mazurek.93@gmail.com>
2020-02-07 07:38:51 +0100

    Add pagination

--- main.js
+++ main.js
@@ -1,79 +1,79 @@
-"use strict"
+"use strict";
 
-const table = document.querySelector('.table--js');
-const pagination = document.querySelector('.pagination--js');
+const table = document.querySelector(".table--js");
+const pagination = document.querySelector(".pagination--js");
 
 let options = {
-  sort_column: 'id',
-  sort_order: 'asc',
-  filter: '',
+  sort_column: "id",
+  sort_order: "asc",
+  filter: "",
   page_size: 5,
-  page: 1,
+  page: 1
 };
 
-function handleChange(id, value){
-  options[id]= value;
-  console.log(options);
+function handleChange(id, value) {
+  options[id] = value;
   fetchData();
 }
 
 function handleClick(i) {
   options.page = i;
-  console.log(1234);
-  
+  fetchData();
 }
 
-function fetchData(){
-  const {sort_column, sort_order, filter, page_size, page} = options;
-  fetch(`http://rt.ex7.pl/get-data?sort_column=${sort_column}&sort_order=${sort_order}&filter=${filter}&page_size=${page_size}&page=${page}`, {method: 'POST'})  
-  .then(res => res.json())
-  .then(data => {
-
-    const tableRows = Array.from(data.map(row => {
-      return (
-        `<tr>
+function fetchData() {
+  const { sort_column, sort_order, filter, page_size, page } = options;
+  fetch(
+    `http://rt.ex7.pl/get-data?sort_column=${sort_column}&sort_order=${sort_order}&filter=${filter}&page_size=${page_size}&page=${page}`,
+    { method: "POST" }
+  )
+    .then(res => res.json())
+    .then(data => {
+      const tableRows = Array.from(
+        data.map(row => {
+          return `<tr>
           <td>${row.id}</td>
           <td>${row.acronym}</td>
           <td>${row.name}</td>
-        </tr>`
-      )
-    })).join('');
+        </tr>`;
+        })
+      ).join("");
 
-    table.innerHTML = `
+      table.innerHTML = `
           <tr class="table__header">
             <th class="table__id scope="col">Id</th>
             <th class="table__acronym scope="col">Acronym</th>
             <th class="table__name scope="col">Name</th>
           </tr>
-          ${ tableRows }
+          ${tableRows}
     `;
 
+      fetch("http://rt.ex7.pl/get-data?page_size=1000", { method: "POST" })
+        .then(res => res.json())
+        .then(data => {
+          const pagesAmount = Math.ceil(data.length / options.page_size);
 
-//     var paramsString = `q=URLUtils.searchParams&page_size=${page_size}&page=${page}`;
-// var searchParams = new URLSearchParams(paramsString);
+          pagination.innerHTML = "";
 
-// //Iterate the search parameters.
-// for (let p of searchParams) {
-//   console.log(p);
-// }
-
-    fetch("http://rt.ex7.pl/get-data?page_size=1000", {method: 'POST'})
-      .then(res => res.json())
-      .then(data => {
-        const pagesAmount = Math.ceil(data.length/options.page_size);
-        console.log(options.page)
-
-        for(let i = 1; i <= pagesAmount; i++){
-          pagination.innerHTML = pagination.innerHTML + `
-            <button  class="pagination__button" onclick="${() => handleClick(i)}">${i}</button>
+          for (let i = 1; i <= pagesAmount; i++) {
+            pagination.innerHTML =
+              pagination.innerHTML +
+              `
+            <button  class="pagination__button" onclick="handleClick(${i})">${i}</button>
           `;
-        }
-      })
+          }
+        //   var paramsString = `q=URLUtils.searchParams&page=${page}`;
+        //   var searchParams = new URLSearchParams(paramsString);
 
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+        //   //Iterate the search parameters.
+        //   for (let p of searchParams) {
+        //     console.log(p);
+        //   }
+        });
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
 }
 
 fetchData();
